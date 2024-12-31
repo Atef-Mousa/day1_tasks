09fb3d32dd7a1fba12f140e82bb1e209b43439fd
M Lapan <M Lapan@DESKTOP-VM80R2C>
2024-12-31 17:43:52 +0200

    Add project files.

---
+++ task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int smallCarpetPrice = 25;
+            int largeCarpetPrice = 35;
+            double taxRate = 0.06;
+            Console.WriteLine("Estimate for carpet cleaning service");
+            Console.Write("Number of small carpets : ");
+            int smallCarpet = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Number of large carpets : ");
+            int largeCarpet = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Price per small room: 25");
+            Console.WriteLine("Price per large room: 35");
+
+            int cost = ((smallCarpetPrice * smallCarpet) + (largeCarpetPrice * largeCarpet));
+            double tax = (cost * taxRate);
+            double costAfterTax = cost + tax ;
+
+            Console.WriteLine($"Cost : ${cost}");
+            Console.WriteLine($"Tax : ${tax}");
+            Console.WriteLine("===============================");
+            Console.WriteLine($"Total estimate: ${costAfterTax}");
+            Console.WriteLine("This estimate is valid for 30 days");
+        }
+    }
+}
